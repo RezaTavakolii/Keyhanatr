2dc07e656882a62e90da930f810bbec4ae940f71
Reza <tavakoli_r.iran@yahoo.com>
2021-11-07 19:02:47 +0330

    product Admin

--- Keyhanatr.Data/Migrations/20211107134132_product.Designer.cs
+++ Keyhanatr.Data/Migrations/20211107134132_product.Designer.cs
@@ -4,14 +4,16 @@ using Keyhanatr.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Keyhanatr.Data.Migrations
 {
     [DbContext(typeof(KeyhanatrContext))]
-    partial class KeyhanatrContextModelSnapshot : ModelSnapshot
+    [Migration("20211107134132_product")]
+    partial class product
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
